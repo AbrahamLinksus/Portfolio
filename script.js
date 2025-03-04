d1626736973b3a9f8f5d11f1bb68fdfa77a908dc
Jake <jakeroberts.a.05@gmail.com>
2025-03-04 21:07:12 +0530

    added framework js

--- script.js
+++ script.js
@@ -14,4 +14,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     observer.observe(text);
 });
-
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach(entry => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
