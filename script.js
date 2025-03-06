922ce3a3d2c8695b5dfec410da0fde970db2bc90
Jake <jakeroberts.a.05@gmail.com>
2025-03-06 21:28:19 +0530

    Changing stats in section 1

--- script.js
+++ script.js
@@ -14,16 +14,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
     observer.observe(text);
 });
-const observer = new IntersectionObserver((entries) =>{
-    entries.forEach(entry => {
-        console.log(entry)
-        if (entry.isIntersecting){
-            entry.target.classList.add("show");
-        } else {
-            entry.target.classList.remove("show");
-        }
-    });
-});
-const hiddenElements = document.querySelectorAll(".section-1, .section-2, .section-3, .section-4");
-hiddenElements.forEach((el) => observer.observe(el));
 
